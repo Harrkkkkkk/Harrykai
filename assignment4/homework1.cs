e3c13713900ad1a916ba08a922bea7cabc75fd71
harrytheconqueror <2664281615@qq.com>
2025-03-19 14:52:10 +0800

    第四次作业

---
+++ assignment4/homework1.cs
@@ -0,0 +1,102 @@
+﻿/*using System;
+
+public class Node<T>
+{
+    public T Data;
+    public Node<T> Next;
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    private Node<T> head;
+
+    public LinkedList()
+    {
+        head = null;
+    }
+
+    public void Add(T data)
+    {
+        var newNode = new Node<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            var current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        var current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new LinkedList<int>();
+
+        Console.WriteLine("请输入整数（输入 'exit' 结束）：");
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+
+            if (int.TryParse(input, out int number))
+            {
+                list.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("无效输入，请输入一个整数。");
+            }
+        }
+
+        // 打印链表元素
+        Console.WriteLine("链表元素:");
+        list.ForEach(x => Console.WriteLine(x));
+
+        // 求最大值
+        int max = int.MinValue;
+        list.ForEach(x => {
+            if (x > max) max = x;
+        });
+        Console.WriteLine($"最大值: {max}");
+
+        // 求最小值
+        int min = int.MaxValue;
+        list.ForEach(x => {
+            if (x < min) min = x;
+        });
+        Console.WriteLine($"最小值: {min}");
+
+        // 求和
+        int sum = 0;
+        list.ForEach(x => sum += x);
+        Console.WriteLine($"求和: {sum}");
+    }
+}
+*/
---
+++ assignment4/homework2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Timers; // 确保使用 System.Timers.Timer
+
+public class AlarmClock
+{
+    private System.Timers.Timer timer; // 使用 System.Timers.Timer
+    private DateTime alarmTime;
+
+    // 嘀嗒事件
+    public event Action Tick;
+    // 响铃事件
+    public event Action Alarm;
+
+    public AlarmClock()
+    {
+        timer = new System.Timers.Timer(1000); // 每秒触发一次
+        timer.Elapsed += OnTick;  // 订阅计时器的 Elapsed 事件
+    }
+
+    // 启动闹钟
+    public void Start(DateTime alarmTime)
+    {
+        this.alarmTime = alarmTime;
+        timer.Start();
+    }
+
+    // 停止闹钟
+    public void Stop()
+    {
+        timer.Stop();
+    }
+
+    // 计时器触发时的处理方法
+    private void OnTick(object sender, ElapsedEventArgs e)
+    {
+        Tick?.Invoke(); // 触发嘀嗒事件
+
+        if (DateTime.Now >= alarmTime)
+        {
+            Alarm?.Invoke(); // 触发响铃事件
+            Stop(); // 停止计时器
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var alarmClock = new AlarmClock();
+
+        // 订阅嘀嗒事件
+        alarmClock.Tick += () => Console.WriteLine("嘀嗒...");
+
+        // 订阅响铃事件
+        alarmClock.Alarm += () => Console.WriteLine("闹钟响铃！时间到了！");
+
+        // 输入设定的时间
+        Console.WriteLine("请输入设定的时间（格式：HH:mm:ss）：");
+        string input = Console.ReadLine();
+
+        // 解析用户输入的时间
+        TimeSpan alarmTimeSpan;
+        if (TimeSpan.TryParse(input, out alarmTimeSpan))
+        {
+            // 设置闹钟时间为当前时间加上用户输入的时间
+            DateTime alarmTime = DateTime.Now.Date.Add(alarmTimeSpan);
+            if (alarmTime <= DateTime.Now) // 如果设定时间在当前时间之前，则加一天
+            {
+                alarmTime = alarmTime.AddDays(1);
+            }
+
+            Console.WriteLine($"闹钟设置在: {alarmTime}");
+            alarmClock.Start(alarmTime);
+        }
+        else
+        {
+            Console.WriteLine("输入的时间格式不正确，请使用 HH:mm:ss 格式。");
+            return;
+        }
+
+        // 保持主线程运行，直到闹钟响铃
+        Console.WriteLine("按下任意键停止闹钟...");
+        Console.ReadKey();
+    }
+}
